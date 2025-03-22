5a2a26a124f39f81fc5c57c587684c81ab44a91e
EliabeBastosDias <eliabebastosdias01@gmail.com>
2025-03-22 17:19:15 -0300

    feat: scroll added

--- script.js
+++ script.js
@@ -1,88 +1,88 @@
 particlesJS('particles-js', {
-    particles: {
-      number: {
-        value: 80,
-        density: {
-          enable: true,
-          value_area: 800
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
         }
       },
-      color: {
-        value: "#219EBC"
+      "color": {
+        "value": "#219EBC"
       },
-      shape: {
-        type: "circle",
-        stroke: {
-          width: 0,
-          color: "#000000"
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
         }
       },
-      opacity: {
-        value: 0.5,
-        random: true,
-        anim: {
-          enable: false,
-          speed: 1,
-          opacity_min: 0.1,
-          sync: false
+      "opacity": {
+        "value": 0.5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
         }
       },
-      size: {
-        value: 3,
-        random: true,
-        anim: {
-          enable: false,
-          speed: 40,
-          size_min: 0.1,
-          sync: false
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
         }
       },
-      line_linked: {
-        enable: true,
-        distance: 150,
-        color: "#FB8500",
-        opacity: 0.4,
-        width: 1
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#FB8500",
+        "opacity": 0.4,
+        "width": 1
       },
-      move: {
-        enable: true,
-        speed: 2,
-        direction: "none",
-        random: false,
-        straight: false,
-        out_mode: "out",
-        bounce: false,
-        attract: {
-          enable: false,
-          rotateX: 600,
-          rotateY: 1200
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
         }
       }
     },
-    interactivity: {
-      detect_on: "canvas",
-      events: {
-        onhover: {
-          enable: true,
-          mode: "grab"
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
         },
-        onclick: {
-          enable: true,
-          mode: "push"
+        "onclick": {
+          "enable": true,
+          "mode": "push"
         },
-        resize: true
+        "resize": true
       },
-      modes: {
-        grab: {
-          distance: 140,
-          line_linked: {
-            opacity: 1
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 1
           }
         },
-        push: {
-          particles_nb: 4
+        "push": {
+          "particles_nb": 4
         }
       }
     },
-    retina_detect: true
+    "retina_detect": true
   });
